605ef8d206536d595f27fe8880826695d855b73b
Lucas Franco <lucassoliveiraa56@gmail.com>
2025-10-15 14:03:10 -0300

    projeto_registro

---
+++ src/projetoregistroads/ProjetoRegistroADS.java
@@ -0,0 +1,11 @@
+package projetoregistroads;
+public class ProjetoRegistroADS {
+
+    public static void main(String[] args) {
+        Registro RegistroFrame = new Registro();
+        RegistroFrame.pack();
+        RegistroFrame.setLocationRelativeTo(null);
+        RegistroFrame.setVisible(true);
+    }
+    
+}
